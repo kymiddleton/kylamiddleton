1819b7d7284b5be4f8aefb1270a1d21727f600dd
Choleslaw <41842273+kymiddleton@users.noreply.github.com>
2019-02-19 21:33:58 -0500

    added new images for project cards

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*=== MODAL PROJECT 1 ===*/
+/*=== PROJECT 1 - VOLUNTEER MOMMY ===*/
 
 //Get the modal
 const modal = document.getElementById("volunteerModal");
@@ -7,7 +7,7 @@ const modal = document.getElementById("volunteerModal");
 const btn = document.getElementById("volunteerBtn");
 
 //Button <span> element to close modal
-const span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("volunteerClose")[0];
 
 //Button to cancel out of modal
 const cancel = document.getElementById("cancel-btn-volunteer");
@@ -35,137 +35,151 @@ window.onclick = function (event) {
 }
 
 
-/*=== PROJECT 2 ===*/
-
-//Get the modal
+/*=== PROJECT 2 PACK IT ===*/
 const packModal = document.getElementById('packModal');
-
-//Button to open modal
 const packBtn = document.getElementById('packBtn');
-
-//Button <span> element to close modal
-const packSpan = document.getElementsByClassName('close')[0];
-
-//Button to cancel out of modal
+const packSpan = document.getElementsByClassName('packClose')[0];
 const packCancel = document.getElementById('cancel-btn-pack');
 
-//Event Listeners
 packCancel.onclick = function () {
     packModal.style.display = "none"
 }
-
-//On click...open modal
 packBtn.onclick = function () {
-    modal.style.display = "block";
+    packModal.style.display = "block";
 }
-
-//On click of <span> (x) close modal
-span2.onclick = function () {
-    modal.style.display = "none";
+packSpan.onclick = function () {
+    packModal.style.display = "none";
 }
-
-//On click....outside of modal...close window
 window.onclick = function (event) {
-    if(event.target === modal2) {
-        modal.style.display = "none";
+    if(event.target === packModal) {
+        packModal.style.display = "none";
+    }
+}
+
+/*=== PROJECT 3 MOMENTUM ===*/
+const momModal = document.getElementById('momModal');
+const momBtn = document.getElementById('momBtn');
+const momSpan = document.getElementsByClassName('momClose')[0];
+const momCancel = document.getElementById('cancel-btn-mom');
+
+momCancel.onclick = function () {
+    momModal.style.display = "none"
+}
+momBtn.onclick = function () {
+    momModal.style.display = "block";
+}
+momSpan.onclick = function () {
+    momModal.style.display = "none";
+}
+window.onclick = function (event) {
+    if(event.target === momModal) {
+        momModal.style.display = "none";
+    }
+}
+        
+/*=== PROJECT 4 RECIPE ===*/
+const recipeModal = document.getElementById('recipeModal');
+const recipeBtn = document.getElementById('recipeBtn');
+const recipeSpan = document.getElementsByClassName('recipeClose')[0];
+const recipeCancel = document.getElementById('cancel-btn-recipe');
+
+recipeCancel.onclick = function () {
+    recipeModal.style.display = "none"
+}
+recipeBtn.onclick = function () {
+    recipeModal.style.display = "block";
+}
+recipeSpan.onclick = function () {
+    recipeModal.style.display = "none";
+}
+window.onclick = function (event) {
+    if(event.target === recipeModal) {
+        recipeModal.style.display = "none";
+    }
+}
+        
+
+/*=== PROJECT 5 TODO LIST ===*/
+const todoModal = document.getElementById('todoModal');
+const todoBtn = document.getElementById('todoBtn');
+const todoSpan = document.getElementsByClassName('todoClose')[0];
+const todoCancel = document.getElementById('cancel-btn-todo');
+
+todoCancel.onclick = function () {
+    todoModal.style.display = "none"
+}
+todoBtn.onclick = function () {
+    todoModal.style.display = "block";
+}
+todoSpan.onclick = function () {
+    todoModal.style.display = "none";
+}
+window.onclick = function (event) {
+    if(event.target === todoModal) {
+        todoModal.style.display = "none";
     }
 }
 
 
-        // Jeopardy
-        var jModal = document.getElementById('jeopardyModal');
-        var jBtn = document.getElementById("jeopardyBtn");
-        var jSpan = document.getElementById("jeopardyClose");
-        var jcancel = document.getElementById("jeopardyCancel");
-        jcancel.onclick = function () {
-            jModal.style.display = "none"
-        }
-        jBtn.onclick = function () {
-            jModal.style.display = "block";
-        }
-        jSpan.onclick = function () {
-            jModal.style.display = "none";
-        }
-        window.onclick = function (event) {
-            if (event.target == jModal) {
-                jModal.style.display = "none";
-            }
-        }
+/*=== PROJECT 6 SOCKET IO TODO LIST ===*/
+const socketModal = document.getElementById('socketModal');
+const socketBtn = document.getElementById('socketBtn');
+const socketSpan = document.getElementsByClassName('socketClose')[0];
+const socketCancel = document.getElementById('cancel-btn-socket');
 
+socketCancel.onclick = function () {
+    socketModal.style.display = "none"
+}
+socketBtn.onclick = function () {
+    socketModal.style.display = "block";
+}
+socketSpan.onclick = function () {
+    socketModal.style.display = "none";
+}
+window.onclick = function (event) {
+    if(event.target === socketModal) {
+        socketModal.style.display = "none";
+    }
+}
+      
+/*=== PROJECT 7 DIRECTORY ===*/
+const directoryModal = document.getElementById('directoryModal');
+const directoryBtn = document.getElementById('directoryBtn');
+const directorySpan = document.getElementsByClassName('directoryClose')[0];
+const directoryCancel = document.getElementById('cancel-btn-directory');
 
-        // League of your imagination
-        var lModal = document.getElementById('leagueModal');
-        var lBtn = document.getElementById("leagueBtn");
-        var lSpan = document.getElementById("leagueClose");
-        var lcancel = document.getElementById("leagueCancel");
-        lcancel.onclick = function () {
-            lModal.style.display = "none"
-        }
-        lBtn.onclick = function () {
-            lModal.style.display = "block";
-        }
-        lSpan.onclick = function () {
-            lModal.style.display = "none";
-        }
-        window.onclick = function (event) {
-            if (event.target == lModal) {
-                lModal.style.display = "none";
-            }
-        }
-        // Restaurant Guide 
-        var rModal = document.getElementById('restModal');
-        var rBtn = document.getElementById("restBtn");
-        var rSpan = document.getElementById("restClose");
-        var rcancel = document.getElementById("restCancel");
-        rcancel.onclick = function () {
-            rModal.style.display = "none"
-        }
-        rBtn.onclick = function () {
-            rModal.style.display = "block";
-        }
-        rSpan.onclick = function () {
-            rModal.style.display = "none";
-        }
-        window.onclick = function (event) {
-            if (event.target == lModal) {
-                rModal.style.display = "none";
-            }
-        }
-        // Waka Tata Flame 
-        var jModal = document.getElementById('wtfModal');
-        var jBtn = document.getElementById("wtfBtn");
-        var jSpan = document.getElementById("wtfClose");
-        var jcancel = document.getElementById("wtfCancel");
-        jcancel.onclick = function () {
-            jModal.style.display = "none"
-        }
-        jBtn.onclick = function () {
-            jModal.style.display = "block";
-        }
-        jSpan.onclick = function () {
-            jModal.style.display = "none";
-        }
-        window.onclick = function (event) {
-            if (event.target == jModal) {
-                jModal.style.display = "none";
-            }
-        }
-        // Beat.box
-        var lModal = document.getElementById('bbModal');
-        var lBtn = document.getElementById("bbBtn");
-        var lSpan = document.getElementById("bbClose");
-        var lcancel = document.getElementById("bbCancel");
-        lcancel.onclick = function () {
-            lModal.style.display = "none"
-        }
-        lBtn.onclick = function () {
-            lModal.style.display = "block";
-        }
-        lSpan.onclick = function () {
-            lModal.style.display = "none";
-        }
-        window.onclick = function (event) {
-            if (event.target == lModal) {
-                lModal.style.display = "none";
-            }
-        }        
+directoryCancel.onclick = function () {
+    directoryModal.style.display = "none"
+}
+directoryBtn.onclick = function () {
+    directoryModal.style.display = "block";
+}
+directorySpan.onclick = function () {
+    directoryModal.style.display = "none";
+}
+window.onclick = function (event) {
+    if(event.target === directoryModal) {
+        directoryModal.style.display = "none";
+    }
+}
+
+/*=== PROJECT 8 FANCY GRID ===*/
+const fancyModal = document.getElementById('fancyModal');
+const fancyBtn = document.getElementById('fancyBtn');
+const fancySpan = document.getElementsByClassName('fancyClose')[0];
+const fancyCancel = document.getElementById('cancel-btn-fancy');
+
+fancyCancel.onclick = function () {
+    fancyModal.style.display = "none"
+}
+fancyBtn.onclick = function () {
+    fancyModal.style.display = "block";
+}
+fancySpan.onclick = function () {
+    fancyModal.style.display = "none";
+}
+window.onclick = function (event) {
+    if(event.target === fancyModal) {
+        fancyModal.style.display = "none";
+    }
+}
